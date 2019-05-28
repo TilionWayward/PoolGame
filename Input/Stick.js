63f6f1c19c40d0c02b2359061269fef482c51101
TilionWayward <tilionthewayward@gmail.com>
2019-05-28 23:24:03 +1000

    Add files via upload

---
+++ Input/Stick.js
@@ -0,0 +1,18 @@
+function Stick(){
+    this.position = {x:0, y:400};
+}
+
+Stick.prototype.update = function(){
+
+    // Testing
+    this.position = Mouse.position;
+
+    if(Mouse.left.pressed){
+        console.log("Pressed left");
+    }
+
+}
+
+Stick.prototype.draw = function(){
+    Canvas.drawImage(sprites.stick, this.position);
+}
